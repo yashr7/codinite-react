43b336c61fc21d4af9836164fc6a17132b36d06b
Yash Raj <yashraj.pratap@gmail.com>
2021-12-01 02:01:26 +0530

    Added Game Cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,51 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+//Redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//Components
+import Game from "../components/Game";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const Home = () => {
+export default function Home() {
   //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  //Get the data back
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
   return (
-    <div className="App">
-      <h1>Hello Ignite</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            key={game.id}
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
-};
+}
 
-export default Home;
+//Styled Components
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
